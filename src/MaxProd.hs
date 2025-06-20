81a30f91601b5735e706545004f9c3329454ccb4
Pierre Thierry <pierre@nothos.net>
2025-06-20 20:03:31 +0200

    Implement naive algorithm

--- src/MaxProd.hs
+++ src/MaxProd.hs
@@ -1,8 +1,21 @@
 module MaxProd where
 
+import Control.Monad (guard, when)
+import Data.Function ((&))
+import Data.List (tails)
+
 maxProd :: [Int] -> Int -> Int
+maxProd nums size | length nums < size = 0
+maxProd nums 1 =
+    maximum $ map (\n -> n * n) nums
 maxProd nums size =
-    go (length nums) nums
-  where
-    go len nums' | len < size = 0
-    go len nums' = undefined
+    maximum $ getProducts nums size
+
+-- get all products of pairs at the extremities of subsequences of this size
+getProducts :: [Int] -> Int -> [Int]
+getProducts nums size = do
+    (start : rest) <- tails nums
+    guard $ length rest >= size - 1
+    end <- drop (size - 2) rest
+    let product = start * end
+    pure product
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,29 @@
 {-# LANGUAGE QuasiQuotes #-}
 
+import Control.Monad (forM_)
 import Data.String.Interpolate (i)
 import MaxProd
 import Test.Hspec
 
+specs :: [([Int], Int, Int)]
+specs =
+    [ ([], 1, 0)
+    , ([1], 1, 1)
+    , ([1, 2, 3, 4], 1, 16)
+    , ([1, 2, -3, 4], 2, 8)
+    , ([-1, -9, 2, 3, -2, -3, 1], 1, 81)
+    , ([1, 3, -5, 5, 6, -4], 3, 20)
+    , ([2, -1, 2, -6, 5, 2, -5, 7], 2, 35)
+    ]
+
 main :: IO ()
 main = hspec $ do
-    describe "naive algorithm" $ do
-        test maxProd [] 1 0
+    testAlg "naive algorithm" maxProd
 
-test :: ([Int] -> Int -> Int) -> [Int] -> Int -> Int -> Spec
-test f nums size expected =
+testAlg :: String -> ([Int] -> Int -> Int) -> Spec
+testAlg label f =
+    describe label $ forM_ specs $ testSpec f
+
+testSpec :: ([Int] -> Int -> Int) -> ([Int], Int, Int) -> Spec
+testSpec f (nums, size, expected) =
     it [i|#{nums}, m=#{size}|] $ f nums size `shouldBe` expected
